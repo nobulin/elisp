6981b86253d631e73adbccf58842764fd68dfb70
nobulin <nobuo.tanaka@mbf.nifty.com>
2017-01-01 21:03:36 +0900

    first commit

---
+++ thread-script.el
@@ -0,0 +1,19 @@
+#!/usr/local/bin/emacs --script
+
+;; Need emacs version 26 or later
+
+(setq __thread-generation 1)
+
+(defun __thread-func ()
+  (dotimes (i 10)
+    (message "Thread Generation %d - %d" __thread-generation i)
+    (sit-for 1))
+  (setq __thread-generation (1+ __thread-generation))
+  (make-thread '__thread-func "__thread-func"))
+
+(__thread-func)
+
+(while (let (result)
+	 (dolist (name (mapcar 'thread-name (all-threads)) result)
+	   (and (string= "__thread-func" name) (setq result t))))
+  (sit-for 1))
