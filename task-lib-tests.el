a37f466ab35e5739ee138486f5eb7e1e2fa19cad
nobulin <nobuo.tanaka@mbf.nifty.com>
2017-01-05 00:40:16 +0900

    some changes

--- task-lib-tests.el
+++ task-lib-tests.el
@@ -4,31 +4,32 @@
 (setq global-variable nil
       global-mutex (make-mutex "global-mutex"))
 
-(let* ((task-1 (create-task "task-1"
-			    '(lambda ()
-			       (let* ((task-id (get-task))
-				      (args (get-task-args task-id)))
-				 (wait-start-task task-id)
-				 (task-cond-wait global-mutex
-						 '(not global-variable))
-				 (set-task-rslt task-id args)))
-			    1 2))
-       (task-2 (create-task "task-2"
-			    '(lambda ()
-			       (let* ((task-id (get-task))
-				      (args (get-task-args task-id)))
-				 (wait-start-task task-id)
-				 (task-cond-notify global-mutex
-						   '(setq global-variable t))
-				 (set-task-rslt task-id args)))
-			    3 4)))
+(let* ((task-1 (create-task
+		"task-1"
+		'(lambda ()
+		   (let* ((args (get-task-param 'args)))
+		     (wait-start-task)
+		     (task-cond-wait global-mutex '(not global-variable))
+		     (set-task-param 'rslt args)))
+		1 2))
+       (task-2 (create-task
+		"task-2"
+		'(lambda ()
+		   (let* ((args (get-task-param 'args)))
+		     (wait-start-task)
+		     (task-cond-notify global-mutex '(setq global-variable t))
+		     (set-task-param 'rslt args)))
+		3 4)))
   (sit-for 3)
-  (start-task task-1)
+  (start-task "task-1")
   (sit-for 3)
-  (start-task task-2)
+  (start-task "task-2")
   (while (or (thread-alive-p (get-task-thrd (get-task "task-1")))
 	     (thread-alive-p (get-task-thrd (get-task "task-2"))))
+    (sit-for 0.5)
     (thread-yield))
-  (exit-task (get-task "task-1"))
-  (exit-task (get-task "task-2"))
+  (put-debug-log (format "task-1 result - %S" (get-task-rslt (get-task "task-1"))))
+  (put-debug-log (format "task-2 result - %S" (get-task-rslt (get-task "task-2"))))
+  (exit-task "task-1")
+  (exit-task "task-2")
   (pop-to-buffer debug-buffer))
