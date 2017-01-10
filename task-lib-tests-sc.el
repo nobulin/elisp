481740fe8267c9b46ec62c4f869925e091923204
Nobuo Tanaka <nobuo.tanaka@mbf.nifty.com>
2017-01-11 00:09:34 +0900

    Add files via upload

--- task-lib-tests-sc.el
+++ task-lib-tests-sc.el
@@ -11,21 +11,24 @@
 	(call-process-shell-command
 	 (format "echo %s; sleep %d" task-name stime))
       (start-process-shell-command
+       "process-sample" nil
        (format "echo %s; sleep %d" task-name stime)))
     (put-debug-log (format "%s is exit" task-name))))
 
-(create-task "process-1" 'process-sample 'call 10)
-(create-task "process-2" 'process-sample 'call 10)
-(create-task "process-3" 'process-sample 'call 10)
-(create-task "process-4" 'process-sample 'call 10)
-(create-task "process-5" 'process-sample 'call 10)
+;; (byte-compile 'process-sample)
+
+(create-task "process-1" 'process-sample 'start 10)
+(create-task "process-2" 'process-sample 'start 10)
+(create-task "process-3" 'process-sample 'start 10)
+(create-task "process-4" 'process-sample 'start 10)
+(create-task "process-5" 'process-sample 'start 10)
 
 (while (not (and (thread-alive-p (get-task-thrd (get-task "process-1")))
 		 (thread-alive-p (get-task-thrd (get-task "process-2")))
 		 (thread-alive-p (get-task-thrd (get-task "process-3")))
 		 (thread-alive-p (get-task-thrd (get-task "process-4")))
 		 (thread-alive-p (get-task-thrd (get-task "process-5")))))
-  (sit-for 0.5)
+  (sit-for 0.1)
   (thread-yield))
 
 (start-task "process-1")
@@ -33,6 +36,14 @@
 (start-task "process-3")
 (start-task "process-4")
 (start-task "process-5")
+
+(while (or (thread-alive-p (get-task-thrd (get-task "process-1")))
+	   (thread-alive-p (get-task-thrd (get-task "process-2")))
+	   (thread-alive-p (get-task-thrd (get-task "process-3")))
+	   (thread-alive-p (get-task-thrd (get-task "process-4")))
+	   (thread-alive-p (get-task-thrd (get-task "process-5"))))
+  (sit-for 0.1)
+  (thread-yield))
 
 (exit-task "process-1")
 (exit-task "process-2")
